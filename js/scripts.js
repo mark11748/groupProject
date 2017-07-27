84fff38fd651cc8dac54ddfab244cad398989630
cs <camdenswita@gmail.com>
2017-07-26 19:31:34 -0700

    starts room description display scripting

--- js/scripts.js
+++ js/scripts.js
@@ -85,13 +85,16 @@
   //player item actions
   Player.prototype.checkCell = function() {
     //clear item display for current cell
+    $("#pcLocation").empty();
     $("div.well p#cellItems").hide();
     $("div.well ul#cellItems").empty();
     //if player's current cell has items
+    var playerVar = this;
+    $("#pcLocation").append(playerVar.cell.description);
     if (this.cell.items.length) {
       $("div.well p#cellItems").show();
       this.cell.items.forEach(function(item){
-        $("div.well ul#cellItems").append("<li class=\"item\">" + item.info.name + "</li>");
+        $("div.well ul#cellItems").text("<li class=\"item\">" + item.info.name + "</li>");
       });
         //once all items displayed-IF there are items- set listeners to act on them
         //listener: player.inventoryAdd
@@ -375,6 +378,27 @@
                                 [[cell_Empty.copyOf(),cell_Empty.copyOf(),cell_Empty.copyOf()],
                                  [cell_Empty.copyOf(),cell_Empty.copyOf(),cell_Empty.copyOf()],
                                  [cell_Empty.copyOf(),cell_Empty.copyOf(),cell_Empty.copyOf()]]);
+  // Give rooms descriptions
+
+  testRoom1.cells[2][2].description = "A slight breeze blows thick dust into your nostrils and you feel the cold damp stone floor as to wake to a darkened room, but where is it?  And how did you get here?  What is the last thing you remember? Do you remember anything?  The air is dense and you are struggling to breathe.  You can make out one door to the west? But why do you know it's the west?";
+
+  testRoom1.cells[2][1].description = "As you walk through the door, you knock over a box and examining the contents strewn across the floor, you find a working flash light that illuminates another door to the west.";
+
+  testRoom1.cells[2][0].description = "This is just another empty chamber, but as you scan the room, your light frames a strange photo, blown up and pasted to the West wall.  Hundreds of people stare masked into the camera.  Uniformed children and adults waiting for the gas to fall. To the south, an open passage is obscured by a neatly stacked pile of ammo crates with a thick layer of dust.";
+
+  testRoom1.cells[1][0].description = "This room is empty, but for the surveillance cameras perched in every corner and over the two doors South and East.  What's behind these doors?";
+
+  testRoom1.cells[1][1].description = "The desks that line this room are loaded down with ancient terminals and on the edge of the farthest from the door, you find a rusty key.";
+
+  testRoom1.cells[0][0].description = "Sloping to the south, pooling water stands stagnant, and black mold crawls up the walls.  This bunker was compromised by something, but the air is still thick and stale... you struggle to breathe... you need to find your way out now.  Run for the door on the East wall.";
+
+  testRoom1.cells[0][1].description = "As you shine your light to the North wall, you see a grizzly sight.  Decomposed bodies, their heads hanging off to the side, and dry pool of blood slowly peeling from the floor leading to the south wall, flooded blood red.  The last body grips a shotgun in it's maggot infested hand.  Will you take the shotgun into the room to the East?";
+
+  testRoom1.cells[0][2].description = "Lockers line the South and East walls, hazmat suits and gas masks... but are they for heading back West or out through the door to the North.";
+
+  testRoom1.cells[1][2].description = "Light! This door to the East leads to the outside world, but what is that outside world, you remember nothing... Better get out there before you choke on the stale air.";
+
+
   //this is the global list containing all rooms
   var roomList = [testRoom1,testRoom2];
   var player1  = new Player("Bob",testRoom1,1,1);
